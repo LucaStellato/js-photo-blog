c125f5729d2fd827fb37c72735c5b70eadf33843
LucaStellato <lucastellato435@gmail.com>
2025-10-27 12:10:37 +0100

    add title to photo in page html

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,13 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
         const cardImage = document.querySelectorAll('.card-image')
         const cardDate = document.querySelectorAll('.date')
         const cardTitle = document.querySelectorAll('.title')
-        replaced.forEach((element,i)=>{
-            if(replaced[i]){
-                const replace=replaced[i]
-                 console.log(replace)
-                 cardImage[i].src=element.url;
-                 cardDate[i].textContent=element.date;
+        replaced.forEach((element, i) => {
+            if (replaced[i]) {
+                const replace = replaced[i]
+                console.log(replace)
+                cardImage[i].src = element.url;
+                cardDate[i].textContent = element.date;
+                cardTitle[i].textContent = element.title;
 
 
 
