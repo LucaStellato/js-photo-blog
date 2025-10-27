cfb956c045d31733cd7bf9de3510d10406ffbd91
LucaStellato <lucastellato435@gmail.com>
2025-10-27 11:35:01 +0100

    bug response not defined

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,8 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
 .then(response=>{
     console.log(response.data)
 })
+const replaced=response.data;
+console.log(replaced)
 
 const cardImage= document.querySelectorAll('card-image')
+
