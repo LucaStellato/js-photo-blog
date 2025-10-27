48411ea36abb1525af9371902f4a19770fabd65d
LucaStellato <lucastellato435@gmail.com>
2025-10-27 11:50:47 +0100

    get date class to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,14 @@ console.log('it work')
 
 axios.get('https://lanciweb.github.io/demo/api/pictures/')
 
-.then(response=>{
-    
-    const replace=response.data
-    console.log(replace)
-    const cardImage= document.querySelectorAll('card-image')
-  
-})
+    .then(response => {
+
+        const replace = response.data
+        console.log(replace)
+        const cardImage = document.querySelectorAll('card-image')
+        const cardDate = document.querySelectorAll('date')
+        const cardTitle = document.querySelectorAll('title')
+    })
 
 
 
