bc2cbcdd643700aec53fc7c9597ae10a76b8091c
LucaStellato <lucastellato435@gmail.com>
2025-10-27 12:04:30 +0100

    visualization image in page html

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,14 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
 
         const replaced = response.data
         console.log(replaced)
-        const cardImage = document.querySelectorAll('card-image')
-        const cardDate = document.querySelectorAll('date')
-        const cardTitle = document.querySelectorAll('title')
-        replaced.forEach((i,element)=>{
+        const cardImage = document.querySelectorAll('.card-image')
+        const cardDate = document.querySelectorAll('.date')
+        const cardTitle = document.querySelectorAll('.title')
+        replaced.forEach((element,i)=>{
             if(replaced[i]){
                 const replace=replaced[i]
                  console.log(replace)
+                 cardImage[i].src=element.url
 
 
 
