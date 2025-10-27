7d39f537b797a11bdfaadb18e9035132f5aea8b9
LucaStellato <lucastellato435@gmail.com>
2025-10-27 11:48:39 +0100

    add div class to insert date and title to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,10 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
 
 .then(response=>{
     
-    console.log(response.data)
     const replace=response.data
     console.log(replace)
     const cardImage= document.querySelectorAll('card-image')
+  
 })
 
 
