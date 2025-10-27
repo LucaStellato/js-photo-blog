3cdc1a86f24dfcc9c435ea81f36613a42caaa7dd
LucaStellato <lucastellato435@gmail.com>
2025-10-27 11:37:35 +0100

    bug resolved

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,14 @@ console.log('it work')
 axios.get('https://lanciweb.github.io/demo/api/pictures/')
 
 .then(response=>{
+    
     console.log(response.data)
+    const replace=response.data
+    console.log(replace)
+    const cardImage= document.querySelectorAll('card-image')
 })
-const replaced=response.data;
-console.log(replaced)
 
-const cardImage= document.querySelectorAll('card-image')
+
+
+
 
