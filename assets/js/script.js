ac05de5ae14b846474a0c0a22423ac82d18a685d
LucaStellato <lucastellato435@gmail.com>
2025-10-27 11:57:31 +0100

    create variablr to cycle array

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,20 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
 
     .then(response => {
 
-        const replace = response.data
-        console.log(replace)
+        const replaced = response.data
+        console.log(replaced)
         const cardImage = document.querySelectorAll('card-image')
         const cardDate = document.querySelectorAll('date')
         const cardTitle = document.querySelectorAll('title')
+        replaced.forEach((i,element)=>{
+            if(replaced[i]){
+                const replace=replaced[i]
+                 console.log(replace)
+
+
+
+            }
+        })
     })
 
 
